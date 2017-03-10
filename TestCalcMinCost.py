308b3331dfb2d26bd678036d50139d6307325326
mark.lei <306697402@qq.com>
2017-03-10 19:27:13 +0800

    Add main part

---
+++ TestCalcMinCost.py
@@ -0,0 +1,112 @@
+﻿# -*- coding: UTF-8 -*-
+# 递归算法：
+# 每次减少三个元素，直到数组中所有的元素都不大于0
+# 
+
+def check_finished(cards_list):
+    # check cards_list 如果所有元素都不大于0说明递归结束
+    for i in cards_list:
+        if i > 0:
+            return False
+    return True
+
+def get_cost(cards_list):
+    tmp = 0
+    for i in cards_list:
+        if i < 0:
+            tmp -= i
+    return tmp
+    
+def try_type_s(cards_list):
+    # copy 一份cards_list
+    copy_list = cards_list[:]
+    # 找出cards_list中第一个大于0的元素的index
+    current_index = 9
+    for i in range(0,9):
+        if copy_list[i] > 0:
+            current_index = i
+            break
+    if current_index < 7:
+        copy_list[current_index] -=1
+        copy_list[current_index+1] -=1
+        copy_list[current_index+2] -=1
+    elif current_index == 7:
+        copy_list[current_index-1] -=1
+        copy_list[current_index] -=1
+        copy_list[current_index+1] -=1
+    else:
+        copy_list[current_index-2] -=1
+        copy_list[current_index-1] -=1
+        copy_list[current_index] -=1
+    return copy_list
+
+def try_type_t(cards_list):
+    # copy 一份cards_list
+    copy_list = cards_list[:]
+    # 找出cards_list中第一个大于0的元素的index
+    current_index = 9
+    for i in range(0,9):
+        if copy_list[i] > 0:
+            current_index = i
+            break
+    copy_list[current_index] -=3
+    return copy_list
+
+# type_s = [1,1,1]
+# type_t = [3]
+# 递归找出最小cost
+def check_s_cost(cards_list):
+    # print cards_list
+    if check_finished(cards_list):
+        return cards_list
+    l_type_s = check_s_cost(try_type_s(cards_list))
+    l_type_t = check_s_cost(try_type_t(cards_list))
+    cost_type_s = get_cost(l_type_s)
+    cost_type_t = get_cost(l_type_t)
+    if cost_type_s > cost_type_t:
+        return l_type_t
+    else:
+        return l_type_s
+
+def check_r_cost(cards_list):
+    l_r_cost=[]
+    min_cost = 99
+    rst_cost = []
+    for i in cards_list:
+        if i > 0:
+            # copy 一份cards_list
+            copy_list = cards_list[:]
+            copy_list[i] -= 2
+            l_r_cost.append(check_s_cost(copy_list))
+    if len(l_r_cost)==0:
+        return [2,0,0,0,0,0,0,0,0]
+    else:
+        for r_cost in l_r_cost:
+            crt_r_cost = get_cost(r_cost)
+            if crt_r_cost < min_cost:
+                rst_cost = r_cost
+                min_cost = crt_r_cost
+        return rst_cost
+# suit_tup = ('dot','bamboo','character')
+def format_checking_list(src_list):
+    dot_list = [0,0,0,0,0,0,0,0,0]
+    bamboo_list = [0,0,0,0,0,0,0,0,0]
+    character_list = [0,0,0,0,0,0,0,0,0]
+    tmp_list = [dot_list,bamboo_list,character_list]
+    for i in src_list:
+        tmp_list[int(i/36)][int((i%36)/4)] += 1
+    return tmp_list
+def check_total_cost(src_list):
+    f_a_list = format_checking_list(src_list)
+    print(f_a_list)
+
+# test_play1 = [14, 18, 35, 36, 41, 45, 46, 50, 58, 60, 63, 66, 84]   
+# check_total_cost(test_play1)
+
+# test_list  =  [0,0,0,0,0,0,0,0,0]
+# a = check_s_cost(test_list)
+# b = check_r_cost(test_list)
+# print test_list
+# print a
+# print b
+
---
+++ TestMahjonghClass.py
@@ -0,0 +1,142 @@
+﻿# -*- coding: UTF-8 -*-
+import random
+from TestCalcMinCost import *
+suit_tup = ('dot','bamboo','character')
+name_arr = ('一筒','二筒','三筒','四筒','五筒','六筒','七筒','八筒','九筒','一条','二条','三条','四条','五条','六条','七条','八条','九条','一万','二万','三万','四万','五万','六万','七万','八万','九万')
+players = []
+def print_cards(cards):
+    a = []
+    b = []
+    str_b = ''
+    for i in cards:
+        # print str(i) + ' , current_size =' + str(len(cards))
+        a.append(i.number)
+    #print a
+    a.sort()
+    print(a)
+    for i in a:
+        b.append(Card.get_name(i))
+    for i in range(0,len(a)):
+        str_b +=' ' + str(a[i]) + ':' + b[i]
+    print(str_b)
+    
+class Card:
+    def __init__(self, number):
+        self.suit = int(number/36)
+        self.value = int((number%36)/4 + 1)
+        self.number = number
+    def __str__(self):
+        return suit_tup[self.suit]+' :' + str(self.value) + '---->number:' + str(self.number)
+    @staticmethod
+    def get_suit(number):
+        return int(number/36)
+    @staticmethod
+    def get_value(number):
+        return  int((number%36)/4 + 1)
+    @staticmethod
+    def get_name(number):
+        return name_arr[int(number/4)]
+
+class CardWalls:
+    cards=[]
+    current_size=0
+    key_cards = []
+    root_card = None 
+    @staticmethod
+    def reset_cards():
+        CardWalls.key_cards = []
+        CardWalls.root_card = None
+        CardWalls.cards=[]
+        random_source = list(range(0,108))
+        for i in range(0,108):
+            # print 'i='+str(i)
+            CardWalls.cards.append(Card(random_source.pop(random.randint(1,108-i)-1)))
+            CardWalls.current_size += 1
+    @staticmethod
+    def print_yourself():
+        print_cards(CardWalls.cards)
+    @staticmethod
+    def deal_card(num):
+        if num == 1:
+            return [CardWalls.cards.pop(0)]
+        elif num == -1:
+            return [CardWalls.cards.pop(-1)]
+        else:
+            return [CardWalls.cards.pop(0),CardWalls.cards.pop(0),CardWalls.cards.pop(0),CardWalls.cards.pop(0)]
+    @staticmethod
+    def get_keycard():
+        CardWalls.root_card = CardWalls.cards.pop(0)
+        key_card_suit = CardWalls.root_card.suit
+        key_card_value = CardWalls.root_card.value%9 # 这里root_card.value已经有加一
+        start_num = key_card_suit*36 + key_card_value*4
+        CardWalls.key_cards = [start_num,start_num+1,start_num+2,start_num+3]
+    
+class Player:
+    def __init__(self,index):
+        self.index = index
+        self.hh_index = 0
+        self.hand_cards = []
+        self.pong_cards = []
+        # Exposed / Concealed Kong
+        self.kong_cards = []
+        self.discard_cards = []
+    def get_handcard_arr(self):
+        tmp_arr=[]
+        for i in self.hand_cards:
+            tmp_arr.append(i.number)
+        tmp_arr.sort()
+        return tmp_arr
+    def reset(self):
+        self.hh_index = 0
+        self.hand_cards = []
+        self.pong_cards = []
+        # Exposed / Concealed Kong
+        self.kong_cards = []
+        self.discard_cards = []
+    def get_4cards(self):
+        self.hh_index += 1
+        self.hand_cards.extend(CardWalls.deal_card(4))
+        # self.hand_cards.sort()
+        #Seq.callnextpalyer()
+    def get_card(self):
+        self.hh_index+=1
+        self.hand_cards.extend(CardWalls.deal_card(1))
+        # self.hand_cards.sort()
+        # if self.hh_index >= 5:
+            # Seq.discardtime()
+        # else:
+            # Seq.callnextpalyer()
+
+def init():
+    if len(players) == 4:
+        return
+    for i in range(0,4):
+        players.append(Player(i))
+
+def start():
+    CardWalls.reset_cards()
+    CardWalls.print_yourself()
+    for i in players:
+        i.reset()
+    # 1st step 4 cards * 3 times
+    for i in range(0,3):
+        for p in players:
+            p.get_4cards()
+    # 2nd step 1 card
+    for p in players:
+            p.get_card()
+    # 3nd get key card
+    CardWalls.get_keycard()
+
+init()
+start()
+for i in players:
+    print('player '+str(i.index)+'--->')
+    print_cards(i.hand_cards)
+    print(i.get_handcard_arr())
+    check_total_cost(i.get_handcard_arr())
+print('root : '+ str(CardWalls.root_card))
+print('keys : '+ str(CardWalls.key_cards))
+#print name_arr
+
+
